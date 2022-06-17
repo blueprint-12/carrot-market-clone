35c4466df9eacf4ec80fa9381a74ba7f853a0048
leeminseok22 <104561638+leeminseok22@users.noreply.github.com>
2022-06-17 21:09:50 +0900

    Merge branch 'main' into min

---
+++ src/pages/PostEdit.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PostingForm from "../components/PostingForm";
+const PostEdit = () => {
+  return <PostingForm />;
+};
+
+export default PostEdit;
---
+++ src/pages/Postwrite.js
@@ -0,0 +1,7 @@
+import React from "react";
+import PostingForm from "../components/PostingForm";
+const PostWrite = () => {
+  return <PostingForm />;
+};
+
+export default PostWrite;
